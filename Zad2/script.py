63cd74ad595da65687d39bfde9ef699259144a7f
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-11-02 20:50:44 +0100

    pierwsza czesc zadania - znajdywanie minmaxow i sprawdzanie czy istnieje punkt siodlowy, jesli tak koniec zadania, jesli nie.. trzeba zaimplementowac dalsza czesc

--- Zad2/script.py
+++ Zad2/script.py
@@ -1,9 +1,17 @@
 import numpy as numpy
 
+arrayRowsInfo = []
+arrayColumnsInfo = []
 matrix = numpy.loadtxt('test.txt')
 print (matrix)
 
-def minmax():
+
+class MinmaxInfo:
+    gameValue = 0
+    rowNumber = 0
+    columnNumber = 0
+
+def minMaxForRows():
     row_min = []
     row_min_column_index_info = []
     
@@ -17,12 +25,42 @@ def minmax():
     max_val = max(row_min)
 
     #check value position in rows
-    max_val_index_i, = numpy.where(row_min == max_val)
+    i, = numpy.where(row_min == max_val)
     #check value position in columns
-    max_val_index_j = row_min_column_index_info[max_val_index_i[0]]
+    j = row_min_column_index_info[i[0]]
 
-    print("Maxmin value is %d from row %d, column %d" % (max_val, max_val_index_i[0]+1, max_val_index_j+1))
-    
-minmax()
+    print("Maxmin value is %d from row %d, column %d" % (max_val, i[0]+1, j+1))
+    arrayInfo = [max_val, i[0]+1, j+1]
+    return arrayInfo
+def maxMinForColumns():
+    column_max = []
+    columnsNumber = matrix.shape[1]
+    rowMaxIdxInfo = []
 
-    
+    for i in range(columnsNumber):
+        maxInRow = max(matrix[:,i])
+        column_max.append(maxInRow)
+        #check row index of the max value
+        idx, = numpy.where(matrix[:,i] == maxInRow)
+        rowMaxIdxInfo.append(idx[0])
+
+    #find min value from the max values
+    minVal = min(column_max)
+
+    #check value position in rows
+    j, = numpy.where(column_max == minVal)
+    i = rowMaxIdxInfo[j[0]]
+    print("Minmax value is %d from row %d, column %d" % (minVal, i+1, j[0]+1))
+    arrayInfo = [minVal, i+1, j[0]+1]
+    return arrayInfo
+
+def checkPunktSiodlowy(rowsInfo, columnsInfo):
+    if rowsInfo[0] == columnsInfo[0]:
+        print("Wartość puntku siodlowego wynosi %d" % rowsInfo[0])
+    if rowsInfo[1] == columnsInfo[1] and rowsInfo[2] == columnsInfo[2]:
+        print("Wyrane strategie to %d dla wierszy i %d dla kolumn" % (rowsInfo[1], rowsInfo[2]))
+
+minMaxForRows()
+maxMinForColumns()
+checkPunktSiodlowy(minMaxForRows(), maxMinForColumns())
+#if (arrayRowsInfo[0] == arrayColumnsInfo[0])
