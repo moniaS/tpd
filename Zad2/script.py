84a05f0d3c387844d7718462e604117e6023cc6e
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-11-03 21:00:43 +0100

    zaimplementowana czesc sprawdzania czy sa dominujace kolumny lub wiersze

--- Zad2/script.py
+++ Zad2/script.py
@@ -2,7 +2,8 @@ import numpy as numpy
 
 arrayRowsInfo = []
 arrayColumnsInfo = []
-matrix = numpy.loadtxt('test.txt')
+matrix = numpy.loadtxt('wariant1.txt')
+optimizedMatrix = []
 print (matrix)
 
 
@@ -32,6 +33,7 @@ def minMaxForRows():
     print("Maxmin value is %d from row %d, column %d" % (max_val, i[0]+1, j+1))
     arrayInfo = [max_val, i[0]+1, j+1]
     return arrayInfo
+
 def maxMinForColumns():
     column_max = []
     columnsNumber = matrix.shape[1]
@@ -57,10 +59,50 @@ def maxMinForColumns():
 def checkPunktSiodlowy(rowsInfo, columnsInfo):
     if rowsInfo[0] == columnsInfo[0]:
         print("Wartość puntku siodlowego wynosi %d" % rowsInfo[0])
-    if rowsInfo[1] == columnsInfo[1] and rowsInfo[2] == columnsInfo[2]:
-        print("Wyrane strategie to %d dla wierszy i %d dla kolumn" % (rowsInfo[1], rowsInfo[2]))
+        if rowsInfo[1] == columnsInfo[1] and rowsInfo[2] == columnsInfo[2]:
+            print("Wyrane strategie to %d dla wierszy i %d dla kolumn" % (rowsInfo[1], rowsInfo[2]))
+        return True
+    return False
 
-minMaxForRows()
-maxMinForColumns()
-checkPunktSiodlowy(minMaxForRows(), maxMinForColumns())
+#function that search for recessive columns and rows
+def findDominatedRowsAndColumns(matrix):
+    rowOrColumnDeleted = False
+    #check for column domination
+    columnsNumber = matrix.shape[1]
+    tempArray = []
+    for i in range(columnsNumber-2):
+        tempArray = matrix[i,:]
+        for j in range(i+1, columnsNumber-1):
+            print(compareTwoVectors(tempArray, matrix[j,:]))
+            #TODO IF RETURNED 1 OR 2 DELETE CORRESPONDING ROW
+# return 0 if none of vectors is entirely smaller, return 1 if first is smaller, return 2 if second is smaller
+def compareTwoVectors(array1, array2):
+    print("Comparing...")
+    print(array1)
+    print(array2)
+    print("--------------")
+    arraySize = array1.size
+    numberOfSmallerOrEqualElementsInArray1 = 0
+    numberOfSmallerOrEqualElementsInArray2 = 0
+    for i in range(arraySize):
+        if array1[i] <= array2[i]:
+            numberOfSmallerOrEqualElementsInArray1 += 1
+        elif array1[i] >= array2[i]:
+            numberOfSmallerOrEqualElementsInArray2 += 1
+    if numberOfSmallerOrEqualElementsInArray1 == arraySize:
+        return 1
+    elif numberOfSmallerOrEqualElementsInArray2 == arraySize:
+        return 2
+    else:
+        return 0
+findDominatedRowsAndColumns(matrix)
+
+
+#minMaxForRows()
+#maxMinForColumns()
+#if(checkPunktSiodlowy(minMaxForRows(), maxMinForColumns())):
+#    print("Koniec")
+#    quit()
+#else:
+#    print("Brak punktu siodłowego. Aby znaleźć rozwiązanie należey skorzystać z programowania liniowego.")
 #if (arrayRowsInfo[0] == arrayColumnsInfo[0])
