b1b08b3c90174d56ca6eef1e25bc2a49d20326b7
Anna Pietrzak <195680@edu.p.lodz.pl>
2018-12-16 19:00:15 +0100

    skonczone zadanie(?)

--- Zad5/main.py
+++ Zad5/main.py
@@ -5,7 +5,7 @@ import sys
 class Process:
     def __init__(self, name, time):
         self.name = name
-        self.time = time
+        self.time = int(time)
     
     def get_time(self):
         return self.time
@@ -23,11 +23,11 @@ class Machine:
         self.total_processing_time = 0
 
     def get_processList(self):
-        return self.processList
+        return self.process_list
 
     def add_process(self, process):
-        self.processList.append(process)
-        self.total_processing_time = total_processing_time + process.get_time()
+        self.process_list.append(process)
+        self.total_processing_time = self.total_processing_time + process.get_time()
 
     def get_total_processing_time(self):
         return self.total_processing_time
@@ -35,22 +35,33 @@ class Machine:
     def __lt__(self, other):
         return self.total_processing_time < other.total_processing_time
 
-list_of_all_processes = []
-number_of_processors = input("Podaj liczbę procesorów: ")
+    def __str__(self):
+        return 'Procesor ' + str(self.id) + ', procesy :' + str([p.time for p in self.process_list])
 
-file = open("data.txt", "r")
-for line in file:
-    line2 = line.split()
-    process = Process(line2[0], line2[1])
-    list_of_all_processes.append(process)
+def read_processes():
+    list_of_all_processes = []
+    file = open("data.txt", "r")
+    for line in file:
+        line2 = line.split()
+        process = Process(line2[0], line2[1])
+        list_of_all_processes.append(process)
 
-for process in list_of_all_processes:
-    print("Nazwa: " + process.get_name() + ", czas wykonywania: " + process.get_time())
+    print("\nWczytane procesy:")
+    for process in list_of_all_processes:
+        print("Nazwa: " + process.get_name() + ", czas wykonywania: " + str(process.get_time()))
+    return list_of_all_processes
 
-list_of_processors = []
-for i in range(1, int(number_of_processors)+1):
-    machine = Machine(i)
-    list_of_processors.append(machine)
+def create_processors():
+    number_of_processors = input("Podaj liczbę procesorów: ")
+    list_of_processors = []
+    for i in range(1, int(number_of_processors)+1):
+        machine = Machine(i)
+        list_of_processors.append(machine)
+    return list_of_processors
+
+
+list_of_all_processes = read_processes()
+list_of_processors = create_processors()
 
 #tworzymy kolejke priorytetowa z rosnącymi czasami wykonywania procesów
 processes_queue = [(p.get_time(), p) for p in list_of_all_processes]
@@ -61,5 +72,10 @@ machine_queue = [(p.get_total_processing_time(), p) for p in list_of_processors]
 heapq.heapify(machine_queue)
 
 while len(processes_queue):
-    p = heapq.heappop(processes_queue)
-    print(p[1].get_time())
+    process = heapq.heappop(processes_queue)
+    machine = heapq.heappop(machine_queue)
+    machine[1].add_process(process[1])
+    machine_queue.append(machine)
+
+for m in list_of_processors:
+    print(m)
